d15477977cd8e6ec9c557730a6c5653181383674
kysCode <kyranbailey8@gmail.com>
2025-01-30 18:32:17 +0000

    Add QR code to quiz lobby

--- team-chipmunk/quizsite/app/views/lobby_view.py
+++ team-chipmunk/quizsite/app/views/lobby_view.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import Http404
+import qrcode
 from quizsite.app.models import Room
 
 def lobby(request, code):
     try:
         room = Room.objects.get(join_code = code)
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=1,
+        )
+        qr.add_data(request.build_absolute_uri())
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save("quizsite/app/static/images/qr_code.png")
         context = {
             'code': f"Room Code: {room.join_code}",
             'name': f"{room.name}",
