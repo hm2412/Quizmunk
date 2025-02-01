cef84c300c14327968696cb5c7330a335ea66061
Areeb Iqbal <areebiqbal04@gmail.com>
2025-02-01 16:51:22 +0000

    Merge lobby and quiz-join-page

--- team-chipmunk/quizsite/app/views/lobby_view.py
+++ team-chipmunk/quizsite/app/views/lobby_view.py
@@ -1,3 +1,5 @@
+import os
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 import qrcode
@@ -17,17 +19,24 @@ def lobby(request, join_code):
         participant, created = RoomParticipant.objects.get_or_create(room=room, guest_access=guest_access)
 
     participants = RoomParticipant.objects.filter(room=room)
+    qr_code_path = "quizsite/app/static/images/qr_code.png"
 
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=1,
+    try:
+        os.makedirs(os.path.dirname(qr_code_path), exist_ok=True)
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=1,
         )
-    qr.add_data(request.build_absolute_uri())
-    qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white")
-    img.save("quizsite/app/static/images/qr_code.png")
+        qr.add_data(request.build_absolute_uri())
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save(qr_code_path)
+        
+    except Exception as e:
+        messages.error(request, f"QR Code generation failed: {e}") 
+        qr_code_path = None  
 
     #if not room.quiz:
     #    messages.error(request, 'Invalid code!')
@@ -39,6 +48,9 @@ def lobby(request, join_code):
         'quiz': room.quiz,
         'join_code': join_code,
         'participants': participants,  
+        'code': f"Room Code: {room.join_code}",
+        'name': f"{room.name}",
+        'qr_code_path': "/static/images/qr_code.png" if qr_code_path else None,
     }
 
     return render(request, 'lobby.html', context)
