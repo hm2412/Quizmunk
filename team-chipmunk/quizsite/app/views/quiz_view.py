884542cf4d327efdc4026fb829349611661f38c5
Tameem Al-Tamimi <tameem.al-tamimi@kcl.ac.uk>
2025-02-01 18:01:29 +0000

    create an editng dashboard for quizzes where a tutor can add quistions and edit them

--- team-chipmunk/quizsite/app/views/quiz_view.py
+++ team-chipmunk/quizsite/app/views/quiz_view.py
@@ -1,45 +1,89 @@
 from django.shortcuts import redirect,render, get_object_or_404
 from quizsite.app.forms import QuizForm, IntegerInputQuestionForm, TrueFalseQuestionForm
-from quizsite.app.models import Quiz, Question
-from django.http import JsonResponse
-
+from quizsite.app.models import Quiz, IntegerInputQuestion, TrueFalseQuestion
+from django.http import HttpResponse, JsonResponse
+from django.urls import reverse
 
 def create_quiz_view(request):
     form = QuizForm(request.POST or None)
-
+    
     if request.method == 'POST' and form.is_valid():
-        form.save()
-
-    if 'HX-Request' not in request.headers:
-        return render(request, 'create_quiz.html', {'form': form})
-
+        quiz = form.save()
+        if request.headers.get('HX-Request'):
+            response = HttpResponse()
+            response['HX-Redirect'] = reverse('edit_quiz', kwargs={'quiz_id': quiz.id})
+            return response
+        else:
+            return redirect('edit_quiz', quiz_id=quiz.id)
+    
     return render(request, 'partials/create_quiz_form.html', {'form': form})
 
 
 def edit_quiz_view(request, quiz_id):
-    quiz = get_object_or_404(Quiz, ID=quiz_id)
+    quiz = get_object_or_404(Quiz, id=quiz_id)
+    
+    questions_int = list(IntegerInputQuestion.objects.filter(quizID=str(quiz.id)))
+    questions_tf = list(TrueFalseQuestion.objects.filter(quizID=str(quiz.id)))
+    questions = questions_int + questions_tf
+    questions.sort(key=lambda q: q.number)
+    
     if request.method == 'POST':
-        if 'integer' in request.POST:  
+        if 'integer' in request.POST:
             int_form = IntegerInputQuestionForm(request.POST)
             if int_form.is_valid():
                 int_form.save()
+                return redirect('edit_quiz', quiz_id=quiz.id)
+            else:
+                tf_form = TrueFalseQuestionForm(initial={'quizID': str(quiz.id)})
         elif 'true_false' in request.POST:
             tf_form = TrueFalseQuestionForm(request.POST)
             if tf_form.is_valid():
                 tf_form.save()
-    
+                return redirect('edit_quiz', quiz_id=quiz.id)
+            else:
+                int_form = IntegerInputQuestionForm(initial={'quizID': str(quiz.id)})
     else:
-        int_form = IntegerInputQuestionForm(initial={'quizID': quiz_id})
-        tf_form = TrueFalseQuestionForm(initial={'quizID': quiz_id})
-
-    return render(request, 'edit_quiz.html',{
-        'quiz':quiz,
-        'int_form':int_form,
-        'tf_form':tf_form
+        int_form = IntegerInputQuestionForm(initial={'quizID': str(quiz.id)})
+        tf_form = TrueFalseQuestionForm(initial={'quizID': str(quiz.id)})
+    
+    return render(request, 'edit_quiz.html', {
+        'quiz': quiz,
+        'int_form': int_form,
+        'tf_form': tf_form,
+        'questions': questions,
     })
 
 
 def delete_question_view(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    try:
+        question = IntegerInputQuestion.objects.get(pk=question_id)
+    except IntegerInputQuestion.DoesNotExist:
+        question = get_object_or_404(TrueFalseQuestion, pk=question_id)
+    quiz_id = int(question.quizID)
     question.delete()
-    return redirect('edit_quiz', quiz_id=question.quizID)
+    return redirect('edit_quiz', quiz_id=quiz_id)
+
+
+def get_question_view(request, quiz_id):
+    question_id = request.GET.get('question_id')
+    try:
+        question = IntegerInputQuestion.objects.get(pk=question_id)
+        question_type = "integer"
+    except IntegerInputQuestion.DoesNotExist:
+        question = get_object_or_404(TrueFalseQuestion, pk=question_id)
+        question_type = "true_false"
+    data = {
+        "id": question.id,
+        "question_type": question_type,
+        "question_text": question.question_text,
+        "number": question.number,
+        "time": question.time,
+        "quizID": question.quizID,
+    }
+    if question_type == "integer":
+        data["mark"] = question.mark
+        data["correct_answer"] = question.correct_answer
+    else:
+        data["mark"] = question.mark
+        data["is_correct"] = question.is_correct
+    return JsonResponse(data)
--- team-chipmunk/quizsite/urls.py
+++ team-chipmunk/quizsite/urls.py
@@ -20,7 +20,7 @@ from quizsite.app.views.homepage_view import homepage
 from quizsite.app.views.dashboard_view import student_dashboard, tutor_dashboard
 from quizsite.app.views.sign_up_view import sign_up_view
 from quizsite.app.views.login_view import login_view
-from quizsite.app.views.quiz_view import create_quiz_view,edit_quiz_view,delete_question_view
+from quizsite.app.views.quiz_view import create_quiz_view,edit_quiz_view,delete_question_view, get_question_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,6 +30,7 @@ urlpatterns = [
     path('student-dashboard/', student_dashboard, name='student_dashboard'),
     path('tutor-dashboard/', tutor_dashboard, name='tutor_dashboard'),
     path('create-quiz/', create_quiz_view, name='create_quiz'),
-    path('edit-quiz/<str:quiz_id>/', edit_quiz_view, name='edit_quiz'),
+    path('edit-quiz/<int:quiz_id>/', edit_quiz_view, name='edit_quiz'),
     path('delete-question/<int:question_id>/', delete_question_view, name='delete_question'),
+    path('get_question/<int:quiz_id>/', get_question_view, name='get_question'),
 ]
