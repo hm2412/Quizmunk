002135797389779ea596e6e5db3be161a46f38ad
Ahmet Taramis <ahmettaramis@hotmail.com>
2025-02-14 02:29:09 +0000

    New password reset page design, and fixed unauthorised access to pages via back button

--- app/views/password_reset_view.py
+++ app/views/password_reset_view.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from app.forms import PasswordResetForm
+from django.views.decorators.cache import never_cache
 
 @login_required
+@never_cache
 def password_reset(request):
     if request.method == "POST":
         form = PasswordResetForm(request.POST, user=request.user)
         if form.is_valid():
-            # Update the user's password
             request.user.set_password(form.cleaned_data["new_password"])
             request.user.save()
+            update_session_auth_hash(request, request.user)  # Prevents logout
             messages.success(request, "Your password has been successfully changed.")
-            return redirect("profile")  # Redirect to profile page after success
+            return redirect("homepage")
     else:
         form = PasswordResetForm(user=request.user)
 
