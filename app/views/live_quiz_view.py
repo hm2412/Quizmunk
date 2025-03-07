8545642f12a0c214168d83ebf448f80409ec49f5
kysCode <kyranbailey8@gmail.com>
2025-03-07 22:07:15 +0000

    Remove merge statement

--- app/views/live_quiz_view.py
+++ app/views/live_quiz_view.py
@@ -1,4 +1,3 @@
-<<<<<<<<< Temporary merge branch 1
 from django.shortcuts import redirect,render, get_object_or_404
 from app.models import Quiz, RoomParticipant, Room, GuestAccess, Question, IntegerInputQuestion, TrueFalseQuestion, QuizState
 from django.http import HttpResponse, JsonResponse
