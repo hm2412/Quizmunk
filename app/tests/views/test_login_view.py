3041cffa0dfdeeff885ba7f1f36d52f706e8bf6e
HM <hassanmuddassir124@gmail.com>
2025-03-25 13:39:25 +0000

    add login and homepage tests

--- app/tests/views/test_login_view.py
+++ app/tests/views/test_login_view.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from app.forms import LoginForm
 
 User = get_user_model()
 
@@ -23,6 +24,13 @@ class ViewTests(TestCase):
             role=User.TUTOR,
         )
 
+    def test_login_view_get(self):
+    # GET request should render the correct template, and include the login form.
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login.html')
+        self.assertIsInstance(response.context['form'], LoginForm)
+
     def test_login_redirects_authenticated_users(self):
         # Log in as a student
         self.client.login(email_address="student@example.com", password="password123")
@@ -33,3 +41,41 @@ class ViewTests(TestCase):
         self.client.login(email_address="tutor@example.com", password="password123")
         response = self.client.get(reverse("login"))
         self.assertRedirects(response, reverse("tutor_dashboard"))
+
+    def test_invalid_login_email(self):
+        # Invalid credentials lead back to the login page
+        data = {
+            'email_address': 'nonexistent@example.com',
+            'password': 'password123'
+        }
+        response = self.client.post(reverse('login'), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Invalid email or password")
+
+    def test_invalid_login_password(self):
+        # Invalid credentials lead back to the login page
+        data = {
+            'email_address': 'student@example.com',
+            'password': 'wrongpassword'
+        }
+        response = self.client.post(reverse('login'), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Invalid email or password")
+
+    def test_logout_view(self):
+        # Test that logouts work
+        self.client.login(email_address="student@example.com", password="password123")
+        response = self.client.get(reverse("login"))
+        self.assertRedirects(response, reverse("student_dashboard"))
+
+        response = self.client.get(reverse('logout'))
+        self.assertRedirects(response, reverse('login'))
+
+        response = self.client.get(reverse("login"))
+        self.assertContains(response, "Logged out successfully!")
+
+    def test_never_cache(self):
+        # Test that browser doesn't cache any responses
+        response = self.client.get(reverse('login'))
+        self.assertIn('Cache-Control', response)
+        self.assertIn('no-cache', response['Cache-Control'])
