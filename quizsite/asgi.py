e9ebe08b38c11faa897776c571cb4e07a369e067
Halludba71 <smuzahir@outlook.com>
2025-03-26 23:17:04 +0000

    ensure routing import delayed until app registry is ready

--- quizsite/asgi.py
+++ quizsite/asgi.py
@@ -9,35 +9,22 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quizsite.settings')
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from app.routing import websocket_urlpatterns
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quizsite.settings')
 
 #application = get_asgi_application()
+
+def get_websocket_url_patterns(): #delaying import
+    from app.routing import websocket_urlpatterns
+    return websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     'http':get_asgi_application(),
     'websocket':AuthMiddlewareStack(
         URLRouter(
-            websocket_urlpatterns
+            get_websocket_url_patterns()
         )
     )
 })
-
-# HTTP + WebSocket handling
-#application = ProtocolTypeRouter({
-#    "http": get_asgi_application(),  
-#
-#    "websocket": AuthMiddlewareStack(
-#        URLRouter([
-#            # Student WebSocket route
-#            path("ws/student/<str:room_code>/", StudentQuizConsumer.as_asgi()),
-#
-#            # Tutor WebSocket route
-#            path("ws/tutor/<str:room_code>/", TutorQuizConsumer.as_asgi()),
-#        ])
-#    ),
-#})
